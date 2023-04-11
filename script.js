54abd9e44d7233f02efa1c06b618d38325c6b81d
Kunal Vartak <thekunalvartak@gmail.com>
2023-04-11 12:38:03 +0530

    Coverting String to Integer

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function calculateFunction() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
     var results_h1_tag = document.getElementById("results");
+    
     results_h1_tag.innerHTML = "Results - " + num1 + " + " + num2 + " = " + (num1+num2);
     results_h1_tag.style.display = "block";
 }
