6ca26febdceb8a6d3f1fb64c2e49de454bb84e9c
Kunal Vartak <thekunalvartak@gmail.com>
2023-04-11 14:50:42 +0530

    Added Operation Logic

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ function calculateFunction() {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var results_h1_tag = document.getElementById("results");
+    var get_selected_value_from_radio_group = document.querySelector( 'input[name="ORG"]:checked'); 
+
+    if (get_selected_value_from_radio_group != null) {
+        switch(get_selected_value_from_radio_group.value) {
+            case "add":
+                results_h1_tag.innerHTML = "Results - " + num1 + " + " + num2 + " = " + (num1+num2);
+                break;
+            case "sub":
+                results_h1_tag.innerHTML = "Results - " + num1 + " - " + num2 + " = " + (num1-num2);
+                break;
+            case "mul":
+                results_h1_tag.innerHTML = "Results - " + num1 + " x " + num2 + " = " + (num1*num2);
+                break;
+            case "div":
+                results_h1_tag.innerHTML = "Results - " + num1 + " ÷ " + num2 + " = " + (num1/num2);
+                break;
+        }
     
-    results_h1_tag.innerHTML = "Results - " + num1 + " + " + num2 + " = " + (num1+num2);
+    } else if (get_selected_value_from_radio_group == null)
+        results_h1_tag.innerHTML = "Nothing has been selected";
+    
     results_h1_tag.style.display = "block";
 }
